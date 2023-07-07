353d5bba304a3fcf9b0540d5338f02675b7a9580
Ayushpani <ayushpanigrahi84@gmail.com>
2023-07-07 13:06:00 +0530

    2nd commi

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_application_1/home_page.dart';
 import 'firebase_options.dart';
+import 'home_page.dart';
 
 void main() {
  runApp(MyApp());
@@ -14,7 +15,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      
     );
   }
 }
